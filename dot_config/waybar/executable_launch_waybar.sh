67dfdfb16e6e413bdb72f476320ed6cd00064800
Martin Frausing <fra@one.com>
2023-12-29 11:34:34 +0100

    waybar changes

--- dot_config/waybar/executable_launch_waybar.sh
+++ dot_config/waybar/executable_launch_waybar.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-CONFIG_FILES="$HOME/.config/waybar/waybar.conf $HOME/.config/waybar/style.css"
+CONF="$HOME/.config/waybar/waybar.conf"
+CSS="$HOME/.config/waybar/style.css"
+CONFIG_FILES="$CONF $CSS"
 
 trap "/usr/bin/killall waybar" EXIT
 
 while true; do
-    waybar -c $HOME/.config/waybar/waybar.conf -s $HOME/.config/waybar/style.css &
+    waybar -c $CONF -s $CSS &
     inotifywait -e create,modify $CONFIG_FILES
     /usr/bin/killall waybar
     sleep 0.2
