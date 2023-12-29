9eb2ce3ef66bff2e320a9a73711c17178d1a2800
Martin Frausing <fra@one.com>
2023-12-29 06:06:09 +0100

    Add backlight script

---
+++ bin/executable_backlight.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+set -x
+
+brightness() {
+	sudo brightnessctl --device='tpacpi::kbd_backlight' $@
+}
+
+if [ "$#" -eq 0 ]; then
+	OUTPUT=$(brightness "get")
+	if [[ "$OUTPUT" == "0" ]]; then
+		brightness "set" "100%"
+	else
+		brightness "set" "0%"
+	fi
+	exit 0
+fi
+
+brightness "set" "$@"
