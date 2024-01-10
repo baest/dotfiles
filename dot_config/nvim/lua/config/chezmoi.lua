1422435571f757d68c8dfa93f16519d8a84809a8
Martin Frausing <fra@one.com>
2024-01-10 08:48:27 +0100

    Various updates

--- dot_config/nvim/lua/config/chezmoi.lua
+++ dot_config/nvim/lua/config/chezmoi.lua
@@ -1,12 +1,13 @@
- require("chezmoi").setup {
-   edit = {
-     watch = true, -- Set true to automatically apply on save.
-     force = false, -- Set true to force apply. Works only when watch = true.
-   },
-   notification = {
-     on_open = true, -- vim.notify when start editing chezmoi-managed file.
-     on_apply = true, -- vim.notify on apply.
-   },
- }
-
 vim.g['chezmoi#use_tmp_buffer'] = true
+
+require("chezmoi").setup {
+  edit = {
+    watch = true, -- Set true to automatically apply on save.
+    force = false, -- Set true to force apply. Works only when watch = true.
+  },
+  notification = {
+    on_open = true, -- vim.notify when start editing chezmoi-managed file.
+    on_apply = true, -- vim.notify on apply.
+  },
+}
+
