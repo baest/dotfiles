92c945668acecf8be906ff0002516b69adf21058
Martin Frausing <fra@one.com>
2024-01-30 13:20:27 +0100

    various

--- dot_zshrc.d/11_goenv.zsh
+++ dot_zshrc.d/11_goenv.zsh
@@ -3,5 +3,5 @@ export PATH="$GOENV_ROOT/bin:$PATH"
 
 eval "$(goenv init -)"
 
-export PATH="$GOROOT/bin:$PATH"
-export PATH="$PATH:$GOPATH/bin"
+#export PATH="$GOROOT/bin:$PATH"
+#export PATH="$PATH:$GOPATH/bin"
