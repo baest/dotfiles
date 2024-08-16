0cee98c6268dc8d7389ae8a3d3ef41494a8b2bff
Martin Frausing <fra@one.com>
2024-08-16 11:47:10 +0200

    Started change from zinit to znap

--- dot_zshrc.d/00_znap.zsh
+++ dot_zshrc.d/00_znap.zsh
@@ -1,3 +1,3 @@
-[[ -r ~/.znap/znap/znap.zsh ]] ||
+[[ -r ~/.local/share/znap/znap/znap.zsh ]] ||
     git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git ~/.znap/znap
-source ~/.znap/znap/znap.zsh
+source ~/.local/share/znap/znap/znap.zsh
