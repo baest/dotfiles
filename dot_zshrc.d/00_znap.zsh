22900a4fd1bbc4ebf4cafd083a005d047029e997
Martin Frausing <fra@one.com>
2024-08-23 06:19:28 +0200

    znap various

--- dot_zshrc.d/00_znap.zsh
+++ dot_zshrc.d/00_znap.zsh
@@ -1,3 +1,3 @@
 [[ -r ~/.local/share/znap/znap/znap.zsh ]] ||
-    git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git ~/.znap/znap
+    git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git ~/.local/share/znap/znap
 source ~/.local/share/znap/znap/znap.zsh
