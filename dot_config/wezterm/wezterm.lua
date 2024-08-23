0f1425c1a55f6674fb0cfdd5b0bc9247d980dc86
Martin Frausing <fra@one.com>
2024-08-23 06:17:14 +0200

    Change to wezterm as terminal

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,6 +9,9 @@ local config = wezterm.config_builder()
 config.check_for_updates = false
 config.hide_tab_bar_if_only_one_tab = true
 
+-- disable ligatures
+--config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
 config.font =
   wezterm.font('FiraCode Nerd Font Mono')
   --wezterm.font('FiraCode Nerd Font Mono', { weight = 'Medium' })
@@ -26,6 +29,24 @@ config.font =
 --})
 config.font_size = 9.1
 config.color_scheme = 'Solarized (light) (terminal.sexy)'
+
+config.keys = {
+  {
+    key = 'O',
+    mods = 'CTRL',
+    action = wezterm.action.QuickSelectArgs {
+      label = 'open url',
+      patterns = {
+        'https?://\\S+',
+      },
+      action = wezterm.action_callback(function(window, pane)
+        local url = window:get_selection_text_for_pane(pane)
+        wezterm.log_info('opening: ' .. url)
+        wezterm.open_with(url)
+      end),
+    },
+  },
+}
 
 
 -- and finally, return the configuration to wezterm
