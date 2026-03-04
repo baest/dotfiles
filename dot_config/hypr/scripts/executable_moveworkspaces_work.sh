c15bf3ed35709fdbe10e5d92799879348d670448
Martin Frausing <fra@one.com>
2026-03-04 13:44:07 +0100

    hyprland various

---
+++ dot_config/hypr/scripts/executable_moveworkspaces_work.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+hyprctl -q --batch 'dispatch moveworkspacetomonitor 1 1 ; dispatch moveworkspacetomonitor 2 1 ; dispatch moveworkspacetomonitor 3 1 ; dispatch moveworkspacetomonitor 4 1 ; dispatch moveworkspacetomonitor 5 1 ; dispatch moveworkspacetomonitor 6 1 ; dispatch moveworkspacetomonitor 7 2 ; dispatch moveworkspacetomonitor 8 2 ; dispatch moveworkspacetomonitor 9 2 ;'
