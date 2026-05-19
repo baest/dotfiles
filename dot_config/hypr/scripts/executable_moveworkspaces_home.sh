fbf0d29be49ecf01d3afd4fc6ed9b8f6b25d26e8
Martin Frausing <fra@one.com>
2026-05-19 10:47:22 +0200

    hyprland lua config

--- dot_config/hypr/scripts/executable_moveworkspaces_home.sh
+++ dot_config/hypr/scripts/executable_moveworkspaces_home.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-hyprctl -q --batch 'dispatch moveworkspacetomonitor 1 1 ; dispatch moveworkspacetomonitor 2 1 ; dispatch moveworkspacetomonitor 3 1 ; dispatch moveworkspacetomonitor 4 1 ; dispatch moveworkspacetomonitor 5 1 ; dispatch moveworkspacetomonitor 6 1 ; dispatch moveworkspacetomonitor 7 1 ; dispatch moveworkspacetomonitor 8 1 ; dispatch moveworkspacetomonitor 9 1 ;'
+hyprctl eval 'dis = hl.dispatch move=hl.dsp.workspace.move for i = 1, 9 do dis(move({ workspace = i, monitor = 1 })) end'
--- dot_config/hypr/scripts/executable_moveworkspaces_work.sh
+++ dot_config/hypr/scripts/executable_moveworkspaces_work.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-hyprctl -q --batch 'dispatch moveworkspacetomonitor 1 1 ; dispatch moveworkspacetomonitor 2 1 ; dispatch moveworkspacetomonitor 3 1 ; dispatch moveworkspacetomonitor 4 1 ; dispatch moveworkspacetomonitor 5 1 ; dispatch moveworkspacetomonitor 6 1 ; dispatch moveworkspacetomonitor 7 2 ; dispatch moveworkspacetomonitor 8 2 ; dispatch moveworkspacetomonitor 9 2 ;'
+hyprctl eval 'dis = hl.dispatch move=hl.dsp.workspace.move for i = 1, 6 do dis(move({ workspace = i, monitor = 1 })) end for i = 7, 9 do dis(move({ workspace = i, monitor = 2 }))'
