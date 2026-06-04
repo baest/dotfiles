69744433aa0f7932d31228cbea46458eeb8eed6a
Martin Frausing <fra@one.com>
2026-06-04 07:50:52 +0200

    silence

--- dot_config/hypr/scripts/executable_moveworkspaces_home.sh
+++ dot_config/hypr/scripts/executable_moveworkspaces_home.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-hyprctl eval 'dis = hl.dispatch move=hl.dsp.workspace.move for i = 1, 9 do dis(move({ workspace = i, monitor = 1 })) end'
+hyprctl eval -q 'dis = hl.dispatch move=hl.dsp.workspace.move for i = 1, 9 do dis(move({ workspace = i, monitor = 1 })) end'
